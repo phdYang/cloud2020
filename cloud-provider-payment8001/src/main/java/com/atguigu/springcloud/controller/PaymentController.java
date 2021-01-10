3cded7ffe529fbb4e9b58661268bf0d196f808a1
zhaoyang <ph.d_yang@qq.com>
2021-01-10 22:35:16 +0800

    fix log

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -15,15 +16,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("server.port")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment){
         int rs = paymentService.create(payment);
         log.info("create is {}", rs);
 
         if (rs > 0) {
-            return new CommonResult(200, "create success", rs);
+            return new CommonResult(200, "create success, serverPort: " + serverPort, rs);
         } else {
-            return new CommonResult(444, "create fail", rs);
+            return new CommonResult(444, "create fail, serverPort: " + serverPort, rs);
         }
     }
 
